5d8373c34a34d8425bfacb32acc78579a7d680b8
Pravin Aryal <pravinaryal010@gmail.com>
2021-08-18 16:14:14 +0545

    adding main file

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "graph.h"
+
+int main()
+{
+
+    Graph g;
+
+    // checking whether vertex is empty or not initially
+    std::cout << g.is_empty() << std::endl;
+
+    // Adding the vertices
+    g.add_vertex(5);
+    g.add_vertex(2);
+    g.add_vertex(4);
+    g.add_vertex(3);
+
+    // printing the vertices
+    g.print();
+
+    // printing total num of vertices
+    std::cout << g.show_numof_vertices() << std::endl;
+
+    // adding edges between two vertices
+    g.add_edge(2, 3);
+    g.add_edge(5, 2);
+
+    // Printing neighbours
+    g.show_neighbours(5);
+    g.show_neighbours(2);
+
+    // checking whether graph is directed or not since we have implement function for undirected graph it returns false.
+    std::cout << g.is_directed() << std::endl;
+
+    // printing indegree of vertex
+
+    std::cout << "The indegree of vertex 2 is ";
+    std::cout << g.show_indegree(2) << std::endl;
+
+    // printing neighbours of vertex
+    g.show_neighbours(3);
